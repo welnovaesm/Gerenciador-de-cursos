7514aaa6724e623ebd0e2009e0e04406326e4403
welnovaesm <wel.miro@hotmail.com>
2022-02-03 17:42:13 -0300

    treinando lista de objetos

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,23 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
-        String aula1 = "Conhecendo mais de listas";
-        String aula2 = "Modelando a classe Aula";
-        String aula3 = "Trabalhando com Cursos e Sets";
+        Aula a1 = new Aula("Revisando as ArrayLists", 21);
+        Aula a2 = new Aula("Listas de objetos", 18);
+        Aula a3 = new Aula("Relacionamento de listas e objetos", 15);
 
-        ArrayList<String> aulas = new ArrayList<>();
-        aulas.add(aula1);
-        aulas.add(aula2);
-        aulas.add(aula3);
+        ArrayList<Aula> aulas = new ArrayList<>();
+        aulas.add(a1);
+        aulas.add(a2);
+        aulas.add(a3);
+        System.out.println(aulas);
+
+        Collections.sort(aulas, Comparator.comparing(Aula::getTempo));
 
         System.out.println(aulas);
 
-        aulas.remove(0);
-
-        //para cada string aula dentro de aulas, faça
-        for (String aula : aulas) {
-            System.out.println("Aula: " + aula);
-        }
-
-        String primeiraAula = aulas.get(0);
-        System.out.println("A primeira aula é " + primeiraAula);
-
-        for (int i = 0; i < aulas.size(); i++) {
-            System.out.println("aula : " + aulas.get(i));
-        }
-
-        aulas.forEach(aula -> {
-            System.out.println("percorrendo :");
-            System.out.println("Aula " + aula);
-        });
-
-        aulas.add("Aumentando nosso conhecimento");
-        System.out.println(aulas);
-
-        Collections.sort(aulas);
-        System.out.println("Depois de ordenado:");
-        System.out.println(aulas);
+        aulas.sort(Comparator.comparing(Aula::getTempo));
     }
 }
