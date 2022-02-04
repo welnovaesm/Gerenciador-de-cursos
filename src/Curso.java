64db808b1a153d27f15e9d37f671931b79f70e2a
welnovaesm <wel.miro@hotmail.com>
2022-02-04 11:00:31 -0300

    aplicando Set no modelo

--- src/Curso.java
+++ src/Curso.java
@@ -1,12 +1,11 @@
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Curso {
 
     private String nome;
     private String instrutor;
     private List<Aula> aulas = new LinkedList<Aula>();
+    private Set<Aluno> alunos = new HashSet<>();
 
     public Curso(String nome, String instrutor) {
         this.nome = nome;
@@ -40,5 +39,13 @@ public class Curso {
     @Override
     public String toString() {
         return "[Curso: " + nome + ", tempo total: " + this.getTempoTotal() + ", aulas:" + this.getAulas() + "]";
+    }
+
+    public void matricula(Aluno aluno){
+        this.alunos.add(aluno);
+    }
+
+    public Set<Aluno> getAlunos() {
+        return Collections.unmodifiableSet(alunos);
     }
 }
---
+++ src/TestaCursoComAluno.java
@@ -0,0 +1,22 @@
+public class TestaCursoComAluno {
+    public static void main(String[] args) {
+        Curso javaColecoes = new Curso("Dominando as coleções do Java", "Paulo Silveira");
+
+        javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 20));
+        javaColecoes.adiciona(new Aula("Criando uma Aula", 10));
+        javaColecoes.adiciona((new Aula( "Modelando com coleções", 25)));
+
+        Aluno a1 = new Aluno("Rodrigo Turini", 34567);
+        Aluno a2 = new Aluno("Guilherme Santana", 23451);
+        Aluno a3 = new Aluno("Mauricio Kobsch", 24569);
+
+        javaColecoes.matricula(a1);
+        javaColecoes.matricula(a2);
+        javaColecoes.matricula(a3);
+
+        System.out.println("Todos os alunos matriculados: ");
+        javaColecoes.getAlunos().forEach(a -> {
+            System.out.println(a);
+        });
+    }
+}
